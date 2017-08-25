bebf63ff8ceec22fa08cb99bde547743127658ad
mbagic <matija.bagic@reversinglabs.com>
2017-08-25 17:08:01 +0200

    moved artist to different file

---
+++ api_getters/artist.py
@@ -0,0 +1,29 @@
+import json
+import urllib
+
+
+class Artist(object):
+    MUSIC_BRAINZ_URL = "http://musicbrainz.org/ws/2/artist/?query=artist:{formatted_name}&fmt=json"
+
+    def __init__(self, name):
+        self.name = name
+        self._mbid = None
+
+    @property
+    def mbid(self):
+
+        if self._mbid is None:
+            response = urllib.urlopen(self.MUSIC_BRAINZ_URL.format(formatted_name=self.formatted_name))
+
+            data = json.loads(response.read())
+
+            for artist in data['artists']:
+                if self.name.lower() in (artist['name'].lower(), artist['sort-name'].lower()):
+                    self._mbid = artist['id']
+
+        return self._mbid
+
+    @property
+    def formatted_name(self):
+        return urllib.quote(self.name.lower().replace("the", "").strip())
+
